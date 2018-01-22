ca946120c3be980fe968a3859ac8823f6b8c656e
glight2000 <glight2000@163.com>
2018-01-23 01:23:19 +0800

    change theme

--- content/welcome.md
+++
@@ -1,9 +0,0 @@
----
-title: "Hi Hugo."
-date: 2018-01-22T18:26:01+08:00
-draft: true
----
-
-This is first article created by Hugo.
-
-merged!!!
--- resarthugo.sh
+++ resarthugo.sh
@@ -2,5 +2,5 @@ killall hugo
 
 cd /root/apps/hugo/site
 
-nohup hugo server --theme=hyde --buildDrafts=true --bind="127.0.0.1" --port=1313 --baseUrl="http://meemle.com.cn/omi" --appendPort=false > /root/apps/hugo/hugo.log&
+nohup hugo server --buildDrafts=true --bind="127.0.0.1" --port=1313 --baseUrl="http://meemle.com.cn/omi" --disableFastRender --appendPort=false > /root/apps/hugo/hugo.log&
 
